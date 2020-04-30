2d68b4aebc98b6a0bb7c30d21f16f4cf774b912d
unknown <lucusapp@gmail.com>
2020-04-30 13:14:58 +0200

    componentes hasta ion-slide

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children:[
+      {
+      path: 'account',
+      loadChildren: '../avatar/avatar.module#AvatarPageModule'
+      },
+      {
+        path: 'contact',
+        loadChildren: '../list/list.module#ListPageModule'
+      },
+      {
+        path: 'settings',
+        loadChildren: '../infinite-scroll/infinite-scroll#InfiniteScrollPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
